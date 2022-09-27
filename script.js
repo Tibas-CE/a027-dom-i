fc00e758f085a99864b24eda5efcfe299a24c5cf
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-09-26 22:35:36 -0300

    Exercícios prontos

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const frutas = ["laranja", "limão", "uva"];
+
+//Exercíco - 01
+const fruta1 = document.getElementById("fruta-1");
+fruta1.innerHTML = frutas[0];
+const fruta2 = document.getElementById("fruta-2");
+fruta2.innerHTML = frutas[1];
+const fruta3 = document.getElementById("fruta-3");
+fruta3.innerHTML = frutas[2];
+const fruta4 = document.getElementById("fruta-4");
+
+//Exercício - 02
+let recebeFruta = document.getElementById("addFruta");
+
+//Exercício - 03
+function addFruta() {
+    fruta4.innerHTML = recebeFruta.value;
+    recebeFruta.value = "";
+};
